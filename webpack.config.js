033f4be6eab09ae6cd4347dd77f69feca4adc0c5
Ramiro Zapata <ramiro.zapata@nan-labs.com>
2017-06-15 17:36:25 -0300

    react working with es6 support

--- webpack.config.js
+++ webpack.config.js
@@ -12,5 +12,16 @@ module.exports = {
     compress: true,
     port: 9000
   },
-  plugins: [new HtmlWebpackPlugin()]
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ]
 }
