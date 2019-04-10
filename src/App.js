cc32ca4089ba99324f90cc5929d63fd34209b728
Ramiro Zapata <ramaxzapata@gmail.com>
2019-04-10 23:29:51 +0100

    organize imports

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
 import * as React from "react";
-import { bindActionCreators } from "redux";
-import { connect } from "react-redux";
 import { Route } from "react-router";
-
-import * as gameActions from "./game/gameActions";
 import PlayerInput from "./game/PlayerInput";
 
-function mapStateToProps(state) {
-  return {
-    players: state.players
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators(gameActions, dispatch);
-}
-
-const PlayerComponent = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(PlayerInput);
-
-const App = () => <Route path="/" component={PlayerComponent} />;
+const App = () => <Route path="/" component={PlayerInput} />;
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-
-import store from "./store";
 import App from "./App";
+import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
