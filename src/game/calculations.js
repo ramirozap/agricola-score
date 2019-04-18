a13c4098bac3e8caea7b3663788bc35405ed72ad
Ramiro Zapata <ramaxzapata@gmail.com>
2019-04-19 01:29:25 +0200

    frst step to implement selectors

--- src/game/calculations.js
+++ src/game/calculations.js
@@ -1,78 +1,85 @@
-export const fields = (total = 0) => {
-  if (total <= 1) {
+import { createSelector } from "reselect";
+
+export const fieldsPoints = (fields = 0) => {
+  if (fields <= 1) {
     return -1;
-  } else if (total === 2) {
-    return 1;
-  } else if (total === 3) {
-    return 2;
-  } else if (total === 4) {
-    return 3;
+  }
+  if (fields <= 4) {
+    return fields - 1;
   }
   return 4;
 };
 
-export const pasturesVegetables = (total = 0) => {
-  if (total >= 1 && total <= 4) {
+export const pasturesVegetablesPoints = (total = 0) => {
+  if (total <= 0) {
+    return -1;
+  }
+  if (total < 4) {
     return total;
-  } else if (total > 4) {
-    return 4;
   }
-  return -1;
+  return 4;
 };
 
-export const grainSheeps = (total = 0) => {
+export const grainSheepsPoints = (total = 0) => {
   if (total < 1) {
     return -1;
-  } else if (total >= 1 && total <= 3) {
+  }
+  if (total <= 3) {
     return 1;
-  } else if (total >= 4 && total <= 5) {
+  }
+  if (total <= 5) {
     return 2;
-  } else if (total >= 6 && total <= 7) {
+  }
+  if (total <= 7) {
     return 3;
   }
   return 4;
 };
 
-export const boars = (total = 0) => {
-  if (total < 1) {
+export const boarsPoints = (boars = 0) => {
+  if (boars < 1) {
     return -1;
-  } else if (total >= 1 && total <= 2) {
+  }
+  if (boars <= 2) {
     return 1;
-  } else if (total >= 3 && total <= 4) {
+  }
+  if (boars <= 4) {
     return 2;
-  } else if (total >= 5 && total <= 6) {
+  }
+  if (boars <= 6) {
     return 3;
   }
   return 4;
 };
 
-export const cattles = (total = 0) => {
-  if (total < 1) {
+export const cattlesPoints = (cattles = 0) => {
+  if (cattles < 1) {
     return -1;
-  } else if (total === 1) {
+  }
+  if (cattles === 1) {
     return 1;
-  } else if (total >= 2 && total <= 3) {
+  }
+  if (cattles >= 2 && cattles <= 3) {
     return 2;
-  } else if (total >= 4 && total <= 5) {
+  }
+  if (cattles >= 4 && cattles <= 5) {
     return 3;
   }
   return 4;
 };
 
-export const linearValue = (total = 0) => total;
+export const unusedSpacesPoints = ({ unusedSpaces }) => unusedSpaces * -1;
 
-export const unusedSpaces = (unusedSpaces = 0) => unusedSpaces * -1;
+export const fencedStablesPoints = ({ fencedStables }) => fencedStables;
 
-export const fencedStables = linearValue;
+export const clayRoomsPoints = ({ clayRooms }) => clayRooms;
 
-export const clayRomms = linearValue;
+export const stoneRoomsPoints = ({ stoneRooms }) => stoneRooms * 2;
 
-export const stoneRooms = (stoneRooms = 0) => stoneRooms * 2;
+export const familyMembersPoints = ({ familyMembers }) => familyMembers * 3;
 
-export const familyMembers = (familyMembers = 0) => familyMembers * 3;
+export const cardPoints = ({ cardPoints }) => cardPoints;
 
-export const cardPoints = linearValue;
+export const bonusPoints = ({ bonusPoints }) => bonusPoints;
 
-export const bonusPoints = linearValue;
-
-export const beggarCards = (total = 0) => total * -3;
+export const beggarCardsPoints = ({ beggarCards }) => beggarCards * -3;
