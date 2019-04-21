6b5960c331862413d0ea62353e2b49b95b7cd796
Ramiro Zapata <ramaxzapata@gmail.com>
2019-04-21 17:18:28 +0200

    typed selectors for points calculations

--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,10 @@
 import { combineReducers, createStore } from 'redux';
-
+import { composeWithDevTools } from 'redux-devtools-extension';
 import gameReducers from './game/gameReducers';
 
 const rootReducer = combineReducers({
   players: gameReducers
 });
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
