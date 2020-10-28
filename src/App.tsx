ccb941ff3b0e40a8d4aac8bdb6a3f8703913bf59
ramirozap <ramaxzapata@gmail.com>
2020-10-28 22:39:04 +0100

    feat: emotion first steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Route } from 'react-router';
 import PlayerInput from './game/PlayerInput';
-import PoinSection from './game/points/PointSection';
+import PointSection from './game/points/PointSection';
 import Result from './game/points/Result';
 import './App.css';
 
@@ -9,7 +9,7 @@ const App = () => {
 	return (
 		<React.Fragment>
 			<Route path="/" exact component={PlayerInput} />
-			<Route path="/points/:pointType" exact component={PoinSection} />
+			<Route path="/points/:pointType" exact component={PointSection} />
 			<Route path="/result" exact component={Result} />
 		</React.Fragment>
 	);
